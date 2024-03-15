1afe386ba466e7f033dc6551f0d179eb4324a627
Luiz <luizkirchbaner.dev@gmail.com>
2024-03-15 15:01:27 -0300

    feat: :tada: initial commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { env } from "./config/env";
+import { buildServer } from "./utils/server";
+
+async function gracefulShutdown({ app }: { app: Awaited<ReturnType<typeof buildServer>> }){
+  await app.close();
+}
+
+async function main(){
+  const app = await buildServer();
+
+  await app.listen({
+    port: env.PORT,
+    host: env.HOST
+  });
+
+  const signals = ['SIGINT', 'SIGTERM'];
+  for(const signal of signals) {
+    process.on(signal, () => {
+      gracefulShutdown({ app })
+    });
+  }
+
+}
+
+main();
